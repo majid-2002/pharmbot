ef3f9964a2b949113d26c5ac5d0d7a1abfbcccc3
Abdul Majid <majid616365@gmail.com>
2024-04-08 02:45:31 +0530

    fix

--- routes/order.js
+++ routes/order.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import orderModel from "../models/order.js";
 
@@ -52,37 +51,3 @@ router.put("/:id", async (req, res) => {
 });
 
 export default router;
-=======
-import orderModel from '../models/order.js'
-import express from "express";
-const app=express()
-
-const router=express.Router()
-
-router.get("/", async (req, res) => {
-    try {
-      const orders = await orderModel.find();
-      res.status(200).json(orders);
-    } catch (error) {
-      res.status(500).json({ error: error });
-    }
-  });
-  
-  router.post("/", async (req, res) => {
-    try {
-      const order = new orderModel({
-        patient: req.body.patient,
-        pharmacy: req.body.pharmacy,
-        status: req.body.status,
-        medicines: req.body.medicines,
-        total: req.body.total,
-      });
-      const newOrder = await order.save();
-      res.status(201).json(newOrder);
-    } catch (error) {
-      res.status(500).json({ error: error });
-    }
-  });
-
-  export default router;
->>>>>>> 55c309511b050909dd42acdb070c887c064bd478
